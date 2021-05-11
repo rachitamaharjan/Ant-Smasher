e832600b3070c63634c74608b572ac386116b9e2
smilerac <smilerac15@gmail.com>
2021-05-12 02:41:29 +0545

    circle bounces back from left side of the container

--- js/script.js
+++ js/script.js
@@ -8,9 +8,6 @@ var points = [
     {x: 200, y: 450},
 ];
 
-var boxContainer = document.getElementsByTagName('canvas');
-
-
 
 
 // points.forEach(function(val, index) {
@@ -27,34 +24,66 @@ var boxContainer = document.getElementsByTagName('canvas');
 //     }
 // });
 
+
+
 function Circle(x, y, r){
     this.x = x;
     this.y = y;
     this.r = r;
-    this.color = '#ff0000';
+    this.color = '#FF8C00';
+    this.movement = Math.random() * Math.PI * 2;
 
-    var circleElement = document.createElement('div');
-    circleElement.setAttribute('class', 'circle');
-    circleElement.style.top = y + 'px';
-    circleElement.style.left = x + "px";
-    boxContainer[0].appendChild(circleElement);
+    // var circleElement = document.createElement('div');
+    // circleElement.setAttribute('class', 'circle');
+    // circleElement.style.top = y + 'px';
+    // circleElement.style.left = x + "px";
+    // boxContainer[0].appendChild(circleElement);
 
-    circleElement.onclick=function deletePoint(){
-        this.parentNode.removeChild(this);
+    // circle.onclick=function deletePoint(){
+        // this.parentNode.removeChild(this);
         // points.splice(index,1);
+    // }
+
+}
+
+Circle.prototype = {
+    updatePosition : function(){
+        this.x += Math.cos(this.movement);
+        this.y += Math.sin(this.movement);
+
+        if(this.x - this.r < 0){
+            this.x = this.r
+            this.movement = Math.atan2(Math.sin(this.movement), Math.cos(this.movement)* -1)
+        }
     }
+};
 
-}(this.bind)
+var boxContainer = document.querySelector('canvas').getContext('2d');
+
+var circle = new Circle(100,200, 30)
+console.log('circle',circle);
 
 
-function loop(){
+
+(function loop(){
     window.requestAnimationFrame(loop);
+    
+
+    circle.updatePosition();
+
+    boxContainer.canvas.width = document.documentElement.clientHeight
+    boxContainer.canvas.height = document.documentElement.clientWidth
+
     boxContainer.fillStyle = circle.color;
     boxContainer.beginPath();
     boxContainer.arc(circle.x, circle.y, circle.r, 0, Math.PI * 2);
     boxContainer.fill();
     
-};
+})();
+// loop()
+
+
+
 
 // var circleArray = Array(100).map((_, i) => i).map(() => new Circle(math.random,10))
 // console.log('circlearray',circleArray)
