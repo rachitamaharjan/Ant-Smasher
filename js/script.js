71aa9b80abf7f0fd458822b6cd15667d1b6bf7c9
smilerac <smilerac15@gmail.com>
2021-05-12 04:28:48 +0545

    added circleArray and completed boundary collision

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ function Circle(x, y, r){
     this.x = x;
     this.y = y;
     this.r = r;
-    this.color = '#FF8C00';
+    this.color = '#'+Math.floor(Math.random()*16777215).toString(16);
     this.movement = Math.random() * Math.PI * 2;
 
     // var circleElement = document.createElement('div');
@@ -46,46 +46,88 @@ function Circle(x, y, r){
 
 }
 
+var boxContainer = document.querySelector('canvas').getContext('2d');
+console.log('hh',boxContainer,'kk' )
+
+
+
 Circle.prototype = {
     updatePosition : function(){
-        this.x += Math.cos(this.movement);
-        this.y += Math.sin(this.movement);
+        this.x += Math.cos(this.movement) * 4;
+        this.y += Math.sin(this.movement) * 4;
 
         if(this.x - this.r < 0){
             this.x = this.r
             this.movement = Math.atan2(Math.sin(this.movement), Math.cos(this.movement)* -1)
         }
+        else if(this.x + this.r > boxContainer.canvas.width){
+            this.x = boxContainer.canvas.width - this.r
+            this.movement = Math.atan2(Math.sin(this.movement), Math.cos(this.movement)* -1)
+        }
+        if(this.y - this.r < 0){
+            this.y = this.r
+            this.movement = Math.atan2(Math.cos(this.movement), Math.sin(this.movement)* -1)
+        }
+        else if(this.y + this.r > boxContainer.canvas.height){
+            this.y = boxContainer.canvas.height - this.r
+            this.movement = Math.atan2(Math.cos(this.movement), Math.sin(this.movement)* -1)
+        }
     }
 };
 
-var boxContainer = document.querySelector('canvas').getContext('2d');
+// var circle = new Circle(boxContainer.canvas.offsetLeft,boxContainer.canvas.offsetTop, 30)
+// console.log('circle',circle);
 
-var circle = new Circle(100,200, 30)
-console.log('circle',circle);
+var circleArray = [];
+for(i = 0; i < 50; i++){
+    circleArray.push(new Circle(boxContainer.canvas.offsetLeft, boxContainer.canvas.offsetTop, 30))
+}
+
 
 
 
 (function loop(){
+    // var circle = circleArray[]
     window.requestAnimationFrame(loop);
     
-
-    circle.updatePosition();
+    // circleArray.forEach(drawCircles)
 
     boxContainer.canvas.width = document.documentElement.clientHeight
     boxContainer.canvas.height = document.documentElement.clientWidth
 
-    boxContainer.fillStyle = circle.color;
-    boxContainer.beginPath();
-    boxContainer.arc(circle.x, circle.y, circle.r, 0, Math.PI * 2);
-    boxContainer.fill();
-    
+    for(i = 0; i < circleArray.length; i++){
+        var circle = circleArray[i]
+        boxContainer.fillStyle = circle.color;
+        boxContainer.beginPath();
+        boxContainer.arc(circle.x, circle.y, circle.r, 0, Math.PI * 2);
+        boxContainer.fill();
+        circle.updatePosition();
+    }
 })();
-// loop()
+
+// (function loop(){
+//     // var circle = circleArray[]
+//     window.requestAnimationFrame(loop);
+    
+//     circleArray.forEach(drawCircles)
+
+//     boxContainer.canvas.width = document.documentElement.clientHeight
+//     boxContainer.canvas.height = document.documentElement.clientWidth
+
+//     function drawCircles(circle){
+//         boxContainer.fillStyle = circle.color;
+//         boxContainer.beginPath();
+//         boxContainer.arc(circle.x, circle.y, circle.r, 0, Math.PI * 2);
+//         boxContainer.fill();
+//         circle.updatePosition();
+//     }
+// })();
 
 
 
 
-// var circleArray = Array(100).map((_, i) => i).map(() => new Circle(math.random,10))
+
+// var circleArray = Array(100).map((_, i) => i).map(() => new Circle())
 // console.log('circlearray',circleArray)
 
 // circleArray.forEach(circle => {
